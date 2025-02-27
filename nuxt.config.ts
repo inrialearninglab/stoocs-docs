909ac0c000e53798876d2b5d43e818526e6f158e
VIAUD Nathan <nathan.viaud@inria.fr>
2025-02-27 17:22:52 +0100

    chores: site url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  extends: ['shadcn-docs-nuxt'],
-  compatibilityDate: '2024-07-06',
+    devtools: { enabled: true },
+    extends: ["shadcn-docs-nuxt"],
+    compatibilityDate: "2024-07-06",
+    site: {
+        url: "https://inrialearninglab.github.io/stoocs-docs",
+    },
 });
